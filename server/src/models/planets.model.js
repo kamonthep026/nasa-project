0b3ba9fcdb53c16ff56f8b575d2707884b4828de
kamonthep <kamonthep026@gmail.com>
2022-04-26 20:46:57 +0700

    Done project

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -5,18 +5,25 @@ const parse = require('csv-parse');
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
-  return planet['koi_disposition'] === 'CONFIRMED' &&
-    planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 &&
-    planet['koi_prad'] < 1.6;
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+  );
 }
 
 function loadPlanetsData() {
   return new Promise((resolve, reject) => {
-    fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
-      .pipe(parse({
-        comment: '#',
-        columns: true,
-      }))
+    fs.createReadStream(
+      path.join(__dirname, '..', '..', 'data', 'kepler_data.csv')
+    )
+      .pipe(
+        parse({
+          comment: '#',
+          columns: true,
+        })
+      )
       .on('data', (data) => {
         if (isHabitablePlanet(data)) {
           habitablePlanets.push(data);
@@ -24,16 +31,20 @@ function loadPlanetsData() {
       })
       .on('error', (err) => {
         console.log(err);
-        reject(err)
+        reject(err);
       })
       .on('end', () => {
         console.log(`${habitablePlanets.length} habitable planets found!`);
-        resolve()
+        resolve();
       });
-  })
+  });
+}
+
+function getAllPlanets() {
+  return habitablePlanets;
 }
 
 module.exports = {
   loadPlanetsData,
-  planets: habitablePlanets
+  getAllPlanets,
 };
